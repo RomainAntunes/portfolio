24c6ce5d169f14a5dde722221a6be4be44a022ad
Romain Antunes <romainantunes2003@hotmail.com>
2023-11-28 21:59:42 +0100

    feat: creating first page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'porfolio';
+export class AppComponent implements OnInit {
+  title = 'Portfolio - Romain Antunes';
+
+  ngOnInit(): void {
+    console.log("Pssst... Looking for the source code? It's here: https://github.com/RomainAntunes/portfolio");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ProjectDetailComponent } from './pages/projects/components/project-detail/project-detail.component';
 import { PersonnalComponent } from './pages/personnal/personnal.component';
+import {NgOptimizedImage} from "@angular/common";
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectsComponent,
     ProjectDetailComponent,
-    PersonnalComponent
+    PersonnalComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
