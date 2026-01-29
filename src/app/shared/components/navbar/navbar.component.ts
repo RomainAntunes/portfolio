991eb155187f257fc8644037fda02f82786a4288
Romain Antunes <romain.antunes@hes-so.ch>
2026-01-29 15:30:59 +0100

    update fix in the typo

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,75 +1,76 @@
-import {Component} from '@angular/core';
-import {NavItem} from "../../models/NavItem";
-import {SocialItem} from "../../models/SocialItem";
-import {MessageService} from "../../services/message.service";
+import { Component } from '@angular/core';
+import { MessageService } from '../../services/message.service';
+import { NavItem } from '../../models/navItem';
+import { SocialItem } from '../../models/socialItem';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
   navItems: NavItem[] = [
     {
       name: 'Home',
       route: '/',
-      local: false
+      local: false,
     },
     {
       name: 'Projects',
       route: '/projects',
-      local: false
+      local: false,
     },
     {
       name: 'Resume (CV)',
       route: '/assets/pdf/CV_Romain_Antunes_FR.pdf',
-      callback: () => this.messageService.add({
-        id: 'resume',
-        title: 'Resume',
-        content: 'Choose your language',
-        smallContent: '* The english version can contain some mistakes, I\'m not a native english speaker',
-        buttons: [
-          {
-            text: 'English',
-            link: '/assets/pdf/CV_Romain_Antunes_EN.pdf',
-            callback: () => window.open('/assets/pdf/CV_Romain_Antunes_EN.pdf', '_blank')
-          },
-          {
-            text: 'French',
-            link: '/assets/pdf/CV_Romain_Antunes_FR.pdf',
-            callback: () => window.open('/assets/pdf/CV_Romain_Antunes_FR.pdf', '_blank')
-          }
-        ]
-      }),
+      callback: () =>
+        this.messageService.add({
+          id: 'resume',
+          title: 'Resume',
+          content: 'Choose your language',
+          smallContent:
+            "* The english version can contain some mistakes, I'm not a native english speaker",
+          buttons: [
+            {
+              text: 'English',
+              link: '/assets/pdf/CV_Romain_Antunes_EN.pdf',
+              callback: () =>
+                window.open('/assets/pdf/CV_Romain_Antunes_EN.pdf', '_blank'),
+            },
+            {
+              text: 'French',
+              link: '/assets/pdf/CV_Romain_Antunes_FR.pdf',
+              callback: () =>
+                window.open('/assets/pdf/CV_Romain_Antunes_FR.pdf', '_blank'),
+            },
+          ],
+        }),
       icon: 'heroArrowTopRightOnSquare',
-      local: true
-    }
+      local: true,
+    },
   ];
 
   socialItems: SocialItem[] = [
     {
       icon: 'ionLogoGithub',
-      route: 'https://github.com/romainantunes'
+      route: 'https://github.com/romainantunes',
     },
     {
       icon: 'ionLogoLinkedin',
-      route: 'https://linkedin.com/in/romainantunes'
+      route: 'https://linkedin.com/in/romainantunes',
     },
     {
       icon: 'ionCodeSlash',
-      route: 'https://codingame.com/profile/defc3ee5279aecc1bb6114e1f994ea9b3325423'
+      route:
+        'https://codingame.com/profile/defc3ee5279aecc1bb6114e1f994ea9b3325423',
     },
     {
       icon: 'ionMailOutline',
-      route: 'mailto:romain.antunes@proton.me'
+      route: 'mailto:romain.antunes@proton.me',
     },
-  ]
+  ];
 
-  constructor(
-    private readonly messageService: MessageService
-  ) {
-  }
+  constructor(private readonly messageService: MessageService) {}
 
   openSpecialCallback(callback: () => void, route: string | undefined) {
     callback();
